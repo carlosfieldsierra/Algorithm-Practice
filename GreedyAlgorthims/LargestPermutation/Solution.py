c20a1121da10764f71f60e1a4d70efccbff7ada9
fieldsierra <fieldsierra@email.arizona.edu>
2022-08-07 20:10:02 -0700

    first

--- GreedyAlgorthims/LargestPermutation/Solution.py
+++ GreedyAlgorthims/LargestPermutation/Solution.py
@@ -7,20 +7,40 @@
     that can be created by executing atmost B swaps.
 '''
 
-def solve(A,B):
-   i = 0
-   _max = len(A)
-   d = {x:i for i,x in enumerate(A)}
 
-   while B and i < len(A):
-        j = d[_max] # Get index of value
-        if i != j:
-            B -= 1
-            A[i], A[j] = A[j], A[i]
+def solve(A,B):
+    numToIndexMap = {num: i for i,num in enumerate(A)}
+
+    index = 0
+    curMaxValue = len(A)
+
+    while B and  index < len(A):
+        
+        maxValIndex = numToIndexMap[curMaxValue]
+
+        if maxValIndex != index:
+            # Flip Array
+            A[maxValIndex],A[index] = A[index], A[maxValIndex]
+    
+            # Flip Dictionary
+            numToIndexMap[A[maxValIndex]],numToIndexMap[A[index]] = \
+                numToIndexMap[A[index]], numToIndexMap[A[maxValIndex]]
+            B -= 1 
             
+            
+        index += 1
+        curMaxValue -= 1
+        
+    
+    return A
+   
+  
+        
+
          
 def Main():
-    solve([4,8,2,7,1],2)
+    ans = solve([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],4)
+    print(ans)
 
 if __name__ == "__main__":
     Main()
